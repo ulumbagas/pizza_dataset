cedf66b7eb1815722278091d4fb854090a3621c0
ulumbagas <ulumbagas@gmail.com>
2023-08-13 15:59:48 +0700

    add total price to Month,Day,date and Hour

--- pizza.sql
+++ pizza.sql
@@ -1,34 +1,78 @@
 -- trends total quantity of pizzas over time
-SELECT 
-    FORMAT_DATE('%B', o.date) AS `Month`, SUM(od.quantity)
-FROM
-    `pizza.order_details` od
-        LEFT JOIN
-    `pizza.orders` o USING (order_id)
-GROUP BY `Month`;
 
-SELECT 
-    FORMAT_DATE('%A', o.date) AS `Day`, SUM(od.quantity)
+--show monhtly Total quantity and Revenue
+SELECT
+  FORMAT_DATE('%B',o.date) AS `Month`,
+  SUM(od.quantity) AS quantity,
+  SUM(od.quantity * p.price) AS Revenue
 FROM
-    `pizza.order_details` od
-        LEFT JOIN
-    `pizza.orders` o USING (order_id)
-GROUP BY `Day`;
+  `pizza.order_details` od
+LEFT JOIN
+  `pizza.orders` o
+ON
+  od.order_id = o.order_id
+LEFT JOIN
+  `pizza.pizzas` p
+ON
+  od.pizza_id = p.pizza_id
+GROUP BY
+  `Month`;
 
-SELECT 
-    EXTRACT(DAY FROM o.date) AS `Date`, SUM(od.quantity)
+--show Dayly Total quantity and Revenue
+SELECT
+  FORMAT_DATE('%A',o.date) AS `Month`,
+  SUM(od.quantity) AS quantity,
+  SUM(od.quantity * p.price) AS Revenue
 FROM
-    `pizza.order_details` od
-        LEFT JOIN
-    `pizza.orders` o USING (order_id)
-GROUP BY `Date`
-ORDER BY `Date`;
+  `pizza.order_details` od
+LEFT JOIN
+  `pizza.orders` o
+ON
+  od.order_id = o.order_id
+LEFT JOIN
+  `pizza.pizzas` p
+ON
+  od.pizza_id = p.pizza_id
+GROUP BY
+  `Month`;
+--show Date Total quantity and Revenue
+SELECT
+  EXTRACT(Day
+  FROM
+    o.date) AS `Date`,
+  SUM(od.quantity),
+  SUM(od.quantity * p.price) AS Revenue
+FROM
+  `pizza.order_details` od
+LEFT JOIN
+  `pizza.orders` o
+ON
+  od.order_id=o.order_id
+LEFT JOIN
+  `pizza.pizzas` p
+ON
+  od.pizza_id = p.pizza_id
+GROUP BY
+  `Date`
+ORDER BY
+  `Date`;
 
-SELECT 
-    EXTRACT(HOUR FROM `time`) AS Hour, SUM(quantity) AS Quantity
+--show Total quantity and Revenue each Hour
+SELECT
+  EXTRACT(HOUR FROM `time`) AS Hour,
+  SUM(od.quantity) AS Quantity,
+  SUM(od.quantity * p.price) AS Revenue
 FROM
-    `pizza.order_details` od
-        JOIN
-    `pizza.orders` o USING (order_id)
-GROUP BY Hour
-ORDER BY Hour;
+  `pizza.order_details` od
+JOIN
+  `pizza.orders` o
+ON
+  od.order_id=o.order_id
+LEFT JOIN
+  `pizza.pizzas` p
+ON
+  od.pizza_id = p.pizza_id
+GROUP BY
+  Hour
+ORDER BY
+  Hour;
