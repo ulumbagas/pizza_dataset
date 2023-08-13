a0f33a12b1167200589d07f938cccfa31aa65b5c
ulumbagas <ulumbagas@gmail.com>
2023-08-13 23:13:56 +0700

    Pizza name Revenue

--- pizza.sql
+++ pizza.sql
@@ -92,3 +92,19 @@ ON
 p.pizza_type_id = pt.pizza_type_id
 group by name
 order by Revenue;
+
+-- Pizza name Revenue
+SELECT
+  pt.category as category, sum(od.quantity) as Total_Quantity, sum(od.quantity * p.price) as Revenue
+FROM
+  `pizza.order_details` od
+LEFT JOIN
+  `pizza.pizzas` p
+ON
+  od.pizza_id = p.pizza_id
+left join 
+ `pizza.pizza_type` pt
+ON
+p.pizza_type_id = pt.pizza_type_id
+group by category
+order by Revenue;
