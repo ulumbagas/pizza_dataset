fb24d2b42e73e9fa4d93afd59241b09663513522
ulumbagas <58242856+ulumbagas@users.noreply.github.com>
2023-09-07 14:28:08 +0700

    Revenue by Category

--- pizza.sql
+++ pizza.sql
@@ -223,5 +223,19 @@ ON
 GROUP BY
   size;
 
-
+--Revenue by Category
+SELECT
+  pt.category as category, sum(od.quantity) as Total_Quantity, ROUND(sum(od.quantity * p.price),2) as Revenue
+FROM
+  `pizza.order_details` od
+LEFT JOIN
+  `pizza.pizzas` p
+ON
+  od.pizza_id = p.pizza_id
+left join 
+ `pizza.pizza_type` pt
+ON
+p.pizza_type_id = pt.pizza_type_id
+group by category
+order by Revenue DESC;
 
